2653e5ac0a960b9293f90649d4d27aa8c77c1e7e
谢贤彬 <xiexianbin@jftech.com>
2024-10-22 14:37:16 +0800

    base init

---
+++ test/xb_calendar_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:xb_calendar/xb_calendar.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
